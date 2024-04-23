3c9dca95afa0054ad4121c28834d8c78698a81eb
Sadam <msadamramli@gmail.com>
2024-04-23 15:49:13 +0700

    penambahan visual

--- index.php
+++ index.php
@@ -1,52 +1,77 @@
+<?php
+//buat supaya jika sudah login tidak bisa mengakses halaman index
+session_start();
+if(isset($_SESSION['login'])){
+  header("Location: profil.php");
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HTML5 Login Form with validation Example</title>
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
     <link rel="stylesheet" href="css/style.css" />
+    <title>Login</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  </head>
-  <body>
-    <div id="login-form-wrap">
-      <h2>Login</h2>
-      <form id="login-form" action="process.php" method="post">
-        <p>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-            onkeyup = "validateEmail()"
-            required
-          />
-        </p>
-        <span id="email-checker"></span>
-        <p>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            onkeyup = "validatePassword()"
-            required
-          />
-        </p>
-        <span id="password-checker"></span>
-        <p>
-          <input
-            type="checkbox"
-            id="remember-me"
-            name="remember-me"
-            value="remember"
-          />
-          <label for="remember-me"> Remember Me</label>
-        </p>
-        <div id="validate-email-pass"></div>
-        <p>
-          <input type="submit" id="login" value="Login" />
-        </p>
-      </form>
+</head>
+<body class="text-center d-flex align-items-center">
+    <div class="wrapper">
+      <main class="form-signin">
+        <form id="login-form" action="process.php" method="post">
+          <div class="container">
+            <h1 class="h3 mb-3 fw-normal">Welcome to Case 3</h1>
+            <h2 class="h3 mb-3 fw-normal">Please sign in</h2>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label"
+                >Email address</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="name@example.com"
+                class="form-control"
+                onkeyup = "validateEmail()"
+                required
+              />
+              <div id="email-checker"></div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label"
+                >Password</label
+              >
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Input your password"
+                class="form-control";
+                onkeyup = "validatePassword()"
+                required
+              />
+              <div id="password-checker"></div>
+            </div>
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" id="checkbox" value="remember-me" />
+                Remember me
+              </label>
+            </div>
+            <div class="col-auto">
+            <input type="submit" id="login" value="Login" class="btn btn-primary mb-3"/>
+              <div id="validate-email-pass"></div>
+            </div>
+          </div>
+        </form>
+      </main>
     </div>
     <script src="js/validateEmail.js"></script>
     <script src="js/validatePass.js"></script>
--- process.php
+++ process.php
@@ -1,11 +1,14 @@
 <?php
-
+session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 function validate($email, $password) {
   
     if ($email == "sadam@gmail.com" && $password == "S@dam123") {
+        //set session
+        $_SESSION['login']=true;
+        $_SESSION['email']="sadam@gmail.com";
         return true;
     } else {
         return false;
